8faa49c935396efaf84ba0a76b80ecfc87c4ae1a
Vladimir <vnb.kh.vnb@gmail.com>
2023-06-26 15:11:35 +0300

    total обнуляю

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,35 +1,59 @@
-import { Typography, Button, ButtonGroup ,Paper} from '@mui/material'
+import { Typography, Button, ButtonGroup } from '@mui/material'
 import './App.scss'
 import Cart from 'components/Cart/Cart'
+import { createContext, useState } from 'react'
+
+type Context = {
+    total: number
+    changTotal: (id: number) => void
+    valuta: string
+}
+
+export const AppContext = createContext<Context | null>(null)
 
 type Props = {}
 const App = (props: Props) => {
-    return (
-        <div className="container row">
-            <Typography variant="h3" component={'h1'} className="text">
-                Our shop page
-            </Typography>
-            <ButtonGroup
-                variant="contained"
-                aria-label="outlined primary button group"
-                className="btn row"
-            >
-                <Button>USD</Button>
-                <Button>EUR</Button>
-                <Button>UAH</Button>
-                <Button>GBP</Button>
-            </ButtonGroup>
-            <div className="row gap">
-            
-                <Cart id={1} />
-                <Cart id={2} />
-                <Cart id={3} />
+    const [total, setTotal] = useState(0)
+    const [valuta, setValuta] = useState('EUR')
 
+    const changTotal = (total: number) => {
+        console.log('changTotal', total)
+        setTotal((prevState) => prevState + total)
+    }
+
+    const changValuta = (valuta: string) => {
+        console.log(valuta, '=valuta')
+        setValuta(() => valuta)
+        setTotal(() => 0)
+    }
+
+    return (
+        <AppContext.Provider
+            value={{ total: total, changTotal: changTotal, valuta: valuta }}
+        >
+            <div className="container row">
+                <Typography variant="h3" component={'h1'} className="text">
+                    Our shop page
+                </Typography>
+                <ButtonGroup
+                    variant="contained"
+                    aria-label="outlined primary button group"
+                    className="btn row"
+                >
+                    <Button onClick={() => changValuta('USD')}>USD</Button>
+                    <Button onClick={() => changValuta('EUR')}>EUR</Button>
+                    <Button onClick={() => changValuta('UAH')}>UAH</Button>
+                    <Button onClick={() => changValuta('GBP')}>GBP</Button>
+                </ButtonGroup>
+                <div className="row gap">
+                    <Cart id={1} />
+                    <Cart id={2} />
+                    <Cart id={3} />
+                </div>
+
+                <Typography variant="h2">total: {total.toFixed(2)} {valuta}</Typography>
             </div>
-            <Typography variant="h2" >
-                total: 2850
-            </Typography>
-        </div>
+        </AppContext.Provider>
     )
 }
 export default App
--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,29 +1,70 @@
-import { Card ,Typography,Button} from "@mui/material"
+import { Card, Typography, Button } from '@mui/material'
 import telefonArray from 'utils/proucttelefon'
+import { useContext } from 'react'
+import { AppContext } from 'Container/App/App'
 
 type Props = {
-    
-    id    :number
+    id: number
 }
-const Cart = ({id}: Props) => {
-    
-  return (
-    <div>
-{telefonArray.filter((item) => item.id === id) 
-.map (
-    ({id,title,titleAll,val,price}) => (
-<Card className="card row">
-            <Typography variant="h3">{title}</Typography>
-            <Typography variant="h5" className="titleAll">{titleAll}</Typography>
-            <Typography variant="h5">{val} {price}</Typography>
+const Cart = ({ id }: Props) => {
+    const data = useContext(AppContext)
+    const krosKurs = (val: string, price: number) => {
+        let priceVal = 0
+        switch (val) {
+            case 'USD':
+                priceVal = price * 1.07
+                break
 
-            <Button  variant="outlined" size="large">Buy</Button>
-        </Card>
+            case 'EUR':
+                priceVal = price * 1
 
+                break
+            case 'UAH':
+                priceVal = price * 39.8
+
+                break
+            case 'GBP':
+                priceVal = price * 0.84
+
+                break
+
+            default:
+                priceVal = price * 1
+
+                break
+        }
+        return priceVal
+    }
+
+    return (
+        <div>
+            {telefonArray
+                .filter((item) => item.id === id)
+                .map(({ id, title, titleAll, val, price }) => (
+                    <Card className="card row" key={id}>
+                        <Typography variant="h3">{title}</Typography>
+                        <Typography variant="h5" className="titleAll">
+                            {titleAll}
+                        </Typography>
+                        <Typography variant="h5">
+                            {data?.valuta!}{' '}
+                            {krosKurs(data?.valuta!, Number(price)).toFixed(2)}
+                        </Typography>
+
+                        <Button
+                            variant="outlined"
+                            size="large"
+                            onClick={() =>
+                                data?.changTotal(
+                                    krosKurs(data?.valuta!, Number(price))
+                                )
+                            }
+                        >
+                            Buy
+                        </Button>
+                    </Card>
+                ))}
+        </div>
     )
-)
-        
-    }    
-  </div> )
 }
 export default Cart
